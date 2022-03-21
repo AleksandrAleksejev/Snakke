6aa31e875d2ec31c1fd6513c2471e0cd12103f66
AleksandrAleksejev <sashaaleksejev813577@gmail.com>
2022-03-21 11:46:55 +0300

    Sounds

---
+++ Snakke/Params.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Snakke
+{
+	public class Params
+	{
+		private string resourcesFoler;
+		public Params()
+		{
+			var ind = Directory.GetCurrentDirectory().ToString()
+				.IndexOf("bin", StringComparison.Ordinal);
+
+			string binFolder =
+				Directory.GetCurrentDirectory().ToString().Substring(0, ind)
+					.ToString();
+
+			resourcesFoler = binFolder + "resources//";
+		}
+		public string GetResourceFolder()
+		{
+			return resourcesFoler;
+		}
+	}
+}
--- Snakke/Sound.cs
+++ Snakke/Sound.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WMPLib;
 
 namespace Snakke
 {
-    class Sound
+    public class Sound
     {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        private string pathToMedia;
+
+        public Sound(string pathToResources)
+        {
+            pathToMedia = pathToResources;
+        }
+        public void Play()
+        {
+            player.URL = pathToMedia + "media.mp3";
+            player.settings.volume = 30;
+            player.controls.play();
+            player.settings.setMode("loop", true);
+
+        }
+
+        public void Play(string songName)
+        {
+            player.URL = pathToMedia + songName + "Game.mp3";
+            player.controls.play();
+
+        }
+
+        public void PlayEat()
+        {
+            player.URL = pathToMedia + "Eat.mp3";
+            player.settings.volume = 100;
+            player.controls.play();
+        }
     }
 }
